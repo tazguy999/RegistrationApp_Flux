ad14c4709b7c2c79739a3a040b174d2f3c4ff7c8
Nick Weber <nweber6@gmail.com>
2018-09-03 21:45:19 -0500

    added links to important examples

--- RiteRegFlux/src/data/programActions.ts
+++ RiteRegFlux/src/data/programActions.ts
@@ -1,4 +1,6 @@
 //https://blog.angular-university.io/angular-2-application-architecture-building-applications-using-rxjs-and-functional-reactive-programming-vs-redux/
+//https://github.com/jhades/angular2-rxjs-single-atom-example/tree/master/src
+//https://github.com/facebook/flux/blob/master/examples/flux-todomvc/src/data/TodoStore.js
 
 import { Program } from './program';
 
